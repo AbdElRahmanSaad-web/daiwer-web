4dae4082f010824a1acf41265241b6df678aa62a
Abdelrahman <youssifehab218@gmail.com>
2024-12-02 11:37:47 +0200

    add logout button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,18 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Dashboard: React.FC = () => <h1>Welcome to the Dashboard</h1>;
+const Dashboard: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("auth_token");
+    navigate("/Login");
+  };
+  return (
+    <>
+      <h1>Welcome to the Dashboard</h1>
+      <button onClick={handleLogout}>logout</button>
+    </>
+  );
+};
 export default Dashboard;
